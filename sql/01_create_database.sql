7ac7e22c80dd3818fcab61207ed2d72d52f90a1a
Balam Singh Rautela <balamrautela17@gmail.com>
2026-02-26 23:12:12 +0530

    Create 01_create_database.sql

---
+++ sql/01_create_database.sql
@@ -0,0 +1,7 @@
+-- ====================================
+-- Pharmacy Inventory Management System
+-- Database And Schema Creation
+-- ====================================
+
+CREATE DATABASE pharmacy_inventory;
+USE pharmacy_inventory;
